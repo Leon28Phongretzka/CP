a945c3cd0fc6320cee7d74fc91bed8fb7d966d16
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-02-26 07:48:58 +0700

    26 02 23

---
+++ SCTDL004.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cerr.tie(0)
+typedef long long ll;
+#define mp make_pair
+#define gl getline
+#define pb push_back
+#define eb emplace_back
+#define ob pop_back
+#define sz(s) ((int)(s.size()))
+#define forn(i,a,b) for (ll i = a; i < b; i++)
+#define forr(i,a,b) for (ll i = a; i >= b; i--)
+#define fora(i, n) for(auto i:n)
+#define Len 100005
+const double pi=3.14159265358979323846;
+const int MOD = 1000000007;
+
+void bin2dec(int n, string a, int i)
+{
+    if(i==n)
+    {
+        for(int j=0; j<n; j++) cout << a[j];
+        cout << " ";
+    }
+    else
+    {
+        a[i]='A';
+        bin2dec(n,a,i+1);
+        a[i]='B';
+        bin2dec(n,a,i+1);
+    }
+}
+
+void solve()
+{
+    int n; cin >> n;
+    vector<int> a(N);
+
+}
+int main()
+{
+    FAST_IO;
+	int tt; cin >> tt; while(tt--) solve();
+	// solve();
+	// freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+}
+
