ef9e7872969d9e7a7926046982a5055ba45e8d82
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-03-10 00:28:26 +0700

    Rennaissance of Rave

--- chal7.js
+++ chal7.js
@@ -1,41 +1,5 @@
-
-function ChallengeClass() {
-    this.ValidateSecretCode = function (secretCode) {
-        var vowels = 0;
-        var ab = false;
-        var cd = false;
-        var pq = false;
-        var xy = false;
-        var dup = false;
-        function checkPassword(password) {
-            for (var i = 0; i < password.length; i++) {
-                if (password[i] == 'a' || password[i] == 'e' || password[i] == 'i' || password[i] == 'o' || password[i] == 'u') {
-                    vowels++;
-                }
-                if (password[i] == 'a' && password[i + 1] == 'b') {
-                    ab = true;
-                }
-                if (password[i] == 'c' && password[i + 1] == 'd') {
-                    cd = true;
-                }
-                if (password[i] == 'p' && password[i + 1] == 'q') {
-                    pq = true;
-                }
-                if (password[i] == 'x' && password[i + 1] == 'y') {
-                    xy = true;
-                }
-                if (password[i] == password[i + 1]) {
-                    dup = true;
-                }
-            }
-            if (vowels >= 3 && ab == false && cd == false && pq == false && xy == false && dup == true) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        return checkPassword(secretCode);
-    }
+class ChallengeClass {
+  ValidateSecretCode(s) {
+    return s.match(/[aeiou]/g).length >= 3 && s.match(/ab|cd|pq|xy/g) == null && s.match(/([a-z])\1/g) != null;
+  }
 }
-
-var result = ChallengeClass.ValidateSecretCode("password");
